1f4a4ff60a434b977bc511a5ebe66991266fefd0
gz2829 <gzucchero24@gmail.com>
2022-01-14 23:20:15 -0600

    add js block plugin

---
+++ plugins/js-block/index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/*
+Plugin Name: JS Block Example
+Plugin URI: http:/stuff.com
+Description: New Block for your Gutten!
+Author: Me
+Version: 1.0.0
+Author URI: https:/g/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class JsBlockExample {
+
+  function __construct(){
+    add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+  }
+
+  function adminAssets(){
+    wp_enqueue_script('newblocktype', plugin_dir_url(__FILE__) . 'app.js', array('wp-blocks'));
+  }
+}
+
+
+$jsBlockExample = new JsBlockExample();
